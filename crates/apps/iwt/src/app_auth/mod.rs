bda665a8c29fcec96dc5dce02e5a2c9ece9c1733
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2020-10-11 06:59:25 +0000

    add a readme

---
+++ crates/apps/iwt/src/app_auth/mod.rs
@@ -0,0 +1,25 @@
+
+use crate::config::Config;
+
+use clap::Subcommand;
+
+mod twitter;
+
+#[derive(Subcommand)]
+pub enum AuthSubcommand {
+    /// Twitter Oauth flow
+    Twitter,
+    /// Mastodon Oauth flow
+    Mastodon,
+}
+
+pub async fn execute(
+    command: AuthSubcommand,
+    config: &Config,
+) -> Result<(), Box<dyn std::error::Error>> {
+    match command {
+        AuthSubcommand::Twitter => twitter::start_flow(config)
+            .await
+            .map_err(|e| Box::new(e) as Box<dyn std::error::Error>),
+        AuthSubcommand::Mastodon => todo!(),
+    }
