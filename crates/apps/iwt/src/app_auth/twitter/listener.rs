f76da181cfd59d6da72cf3ce352d0caedd266d2d
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2020-12-10 12:36:33 +0000

    sit: Fix ipip6_tunnel_lookup device matching criteria

--- crates/apps/iwt/src/app_auth/twitter/listener.rs
+++ crates/apps/iwt/src/app_auth/twitter/listener.rs
@@ -66,4 +66,4 @@ async fn receive_token(
     Query(params): Query<HashMap<String, String>>,
     Extension(state): Extension<Arc<State>>,
 ) -> impl IntoResponse {
-    le
+    let state_param = params.get("stat
