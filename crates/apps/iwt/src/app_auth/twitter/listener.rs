aa41f23c61b5e7fc221af067b066981305ed27c3
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2020-12-12 08:50:08 +0000

    merge from 1.0.0

--- crates/apps/iwt/src/app_auth/twitter/listener.rs
+++ crates/apps/iwt/src/app_auth/twitter/listener.rs
@@ -66,4 +66,6 @@ async fn receive_token(
     Query(params): Query<HashMap<String, String>>,
     Extension(state): Extension<Arc<State>>,
 ) -> impl IntoResponse {
-    let state_param = params.get("state").expect("state para
+    let state_param = params.get("state").expect("state param not found");
+
+    assert!
