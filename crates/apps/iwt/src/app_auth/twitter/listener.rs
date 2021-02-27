f19a46ae0d29b25fb86a8b650cc0c634595a7dea
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-02-27 10:43:36 +0000

    remove debugs

--- crates/apps/iwt/src/app_auth/twitter/listener.rs
+++ crates/apps/iwt/src/app_auth/twitter/listener.rs
@@ -141,4 +141,5 @@ fn persist_tokens(tokens: &TokenResponse, db_path: &String) -> rusqlite::Result<
     conn.execute(
         "INSERT INTO auth_token (social_network, access_token, refresh_token)
          VALUES (?1, ?2, ?3)
-         ON CONFLICT (social_n
+         ON CONFLICT (social_network) 
+            DO 
