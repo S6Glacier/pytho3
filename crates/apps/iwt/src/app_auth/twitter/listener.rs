0013cbfa3ad0c3e7b591a6d8da0b1d5fdce9a7eb
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-02-27 17:42:17 +0000

    fix style issues

--- crates/apps/iwt/src/app_auth/twitter/listener.rs
+++ crates/apps/iwt/src/app_auth/twitter/listener.rs
@@ -142,4 +142,4 @@ fn persist_tokens(tokens: &TokenResponse, db_path: &String) -> rusqlite::Result<
         "INSERT INTO auth_token (social_network, access_token, refresh_token)
          VALUES (?1, ?2, ?3)
          ON CONFLICT (social_network) 
-            DO UPDATE SET access_token = excluded.access_token, refresh_toke
+            DO UPDATE SET access_token = excluded.access_token, refresh_token = excluded.refresh_token",
