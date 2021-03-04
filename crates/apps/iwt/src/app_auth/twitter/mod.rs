4dfe429fcfdec8ddfdb1567cf852fcfa0026c4cf
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-03-04 08:02:13 +0000

    x64

--- crates/apps/iwt/src/app_auth/twitter/mod.rs
+++ crates/apps/iwt/src/app_auth/twitter/mod.rs
@@ -16,3 +16,26 @@ impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "ListenerError")
     }
+}
+
+impl std::error::Error for Error {}
+
+pub async fn start_flow(config: &Config) -> Result<(), Error> {
+    // Create CSRF state and secret challenge
+    let mut challenge = [0u8; 64];
+    let mut csrf_state = [0u8; 64];
+
+    OsRng.fill_bytes(&mut challenge);
+    OsRng.fill_bytes(&mut csrf_state);
+
+    let challenge = base64::encode(challenge);
+    let csrf_state = base64::encode(csrf_state);
+
+    let oauth_uri = construct_uri(&config.twitter.client_id, &csrf_state, &challenge);
+    println!(
+        "Open the following link in your browser:
+
+{}
+",
+        oauth_uri
+    );
