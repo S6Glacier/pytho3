751bd66769dcf1e06d7f053add284c218a68ebe7
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-03-09 18:41:36 +0000

    Added .travis.yml file.

--- crates/apps/iwt/src/commons/auth/token_db.rs
+++ crates/apps/iwt/src/commons/auth/token_db.rs
@@ -32,3 +32,41 @@ impl SqliteTokenDB {
         Self { conn }
     }
 }
+
+impl TokenDB for SqliteTokenDB {
+    fn get_access_token(
+        &self,
+        social_network: &Network,
+    ) -> Result<AccessToken, Box<dyn std::error::Error>> {
+        self.conn
+            .query_row(
+                "SELECT access_token FROM auth_token WHERE social_network = :social_network",
+                &[(":social_network", social_network.to_string().as_str())],
+                |row| row.get("access_token").map(AccessToken::new),
+            )
+            .map_err(|err| Box::new(err) as Box<dyn std::error::Error>)
+    }
+
+    fn get_refresh_token(
+        &self,
+        social_network: &Network,
+    ) -> Result<RefreshToken, Box<dyn std::error::Error>> {
+        self.conn
+            .query_row(
+                "SELECT refresh_token FROM auth_token WHERE social_network = :social_network",
+                &[(":social_network", social_network.to_string().as_str())],
+                |row| row.get("refresh_token").map(RefreshToken::new),
+            )
+            .map_err(|err| Box::new(err) as Box<dyn std::error::Error>)
+    }
+
+    fn store(
+        &self,
+        social_network: &Network,
+        access_token: &AccessToken,
+        refresh_token: &RefreshToken,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.conn.execute(
+            "INSERT INTO auth_token (social_network, access_token, refresh_token)
+             VALUES (?1, ?2, ?3)
+             ON CONFLICT (social_network) 
