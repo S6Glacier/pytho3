a9ba4cb48b384c0f754a19aebc5b9b730f51400d
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-03-15 07:00:10 +0000

    Even more logging

--- crates/apps/iwt/src/commons/mod.rs
+++ crates/apps/iwt/src/commons/mod.rs
@@ -12,3 +12,4 @@ pub struct SqlConversionError {
 
 impl Display for SqlConversionError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("SqlC
