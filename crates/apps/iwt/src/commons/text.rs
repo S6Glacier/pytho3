74ea08bb925cd7bbc36e255c88832a43f1070b4f
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-03-17 19:16:33 +0000

    BUGFIX

--- crates/apps/iwt/src/commons/text.rs
+++ crates/apps/iwt/src/commons/text.rs
@@ -12,3 +12,36 @@ pub fn shorten(text: &str, limit: usize) -> &str {
 
     while i < words.len() && (len + words[i].len() + 1) < limit {
         len += words[i].len() + usize::from(i != 0);
+        i += 1;
+    }
+    &text[0..len]
+}
+
+#[must_use]
+pub fn shorten_with_permashort_citation(
+    text: &str,
+    limit: usize,
+    permashort_citation: &PermashortCitation,
+    tags: &[String],
+) -> String {
+    let hash_tags = tags
+        .iter()
+        .map(|tag| String::from("#") + &tag.to_case(Case::Pascal))
+        .collect::<Vec<_>>()
+        .join(" ");
+
+    let (cleaned, short) = clean_description(text);
+
+    let suffix = if short {
+        format!("\n{hash_tags} {}", permashort_citation.to_uri())
+    } else {
+        format!("\n{hash_tags} ({})", permashort_citation.to_string())
+    };
+
+    let shortened = shorten(&cleaned, limit - suffix.len());
+
+    if shortened == cleaned {
+        let mut appended = cleaned;
+        appended.push_str(&suffix);
+        appended
+    } else {
