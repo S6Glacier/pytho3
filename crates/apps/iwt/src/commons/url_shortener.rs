8fa457893300e59bb26570c708377f5139392cdf
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-04-05 06:53:11 +0000

    remove project

--- crates/apps/iwt/src/commons/url_shortener.rs
+++ crates/apps/iwt/src/commons/url_shortener.rs
@@ -21,4 +21,4 @@ impl From<reqwest::Error> for ClientError {
 
 impl Display for ClientError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(for
+        f.write_fmt(format_args!("UrlShortener Clie
