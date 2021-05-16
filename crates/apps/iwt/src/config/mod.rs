08fb3bd4de9d8bc382616c1aa7a54146d577e3ff
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-05-16 18:48:06 +0000

    After merge fixes

--- crates/apps/iwt/src/config/mod.rs
+++ crates/apps/iwt/src/config/mod.rs
@@ -17,3 +17,46 @@ pub struct Config {
 pub struct Rss {
     pub urls: Vec<String>,
 }
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct DB {
+    pub path: String,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Twitter {
+    pub client_id: ClientId,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Mastodon {
+    pub base_uri: String,
+    pub access_token: AccessToken,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct UrlShortener {
+    pub protocol: String,
+    pub domain: String,
+    pub put_base_uri: Option<String>,
+}
+
+impl PartialEq for Mastodon {
+    fn eq(&self, other: &Self) -> bool {
+        self.base_uri == other.base_uri && self.access_token.secret() == other.access_token.secret()
+    }
+}
+
+impl Config {
+    pub fn from_file(file_name: &str) -> Result<Config, toml::de::Error> {
+        let config_str = fs::read_to_string(file_name)
+            .unwrap_or_else(|_| panic!("Cannot found file: {file_name}"));
+
+        toml::from_str(&config_str)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use oauth2::AccessToken;
+    use oauth2::ClientId;
