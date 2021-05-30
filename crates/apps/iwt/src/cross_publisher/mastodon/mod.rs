30325929da5c56af4d333f1da79bc901b39d4b93
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-05-30 14:36:42 +0000

    Removed unnecessary comments

--- crates/apps/iwt/src/cross_publisher/mastodon/mod.rs
+++ crates/apps/iwt/src/cross_publisher/mastodon/mod.rs
@@ -3,4 +3,5 @@ use std::rc::Rc;
 use super::rss_item_ext::IwtRssExtension;
 use super::syndicated_post::SyndicatedPost;
 use super::target::Target;
-use crate::commons::{text,
+use crate::commons::{text, url_shortener};
+use crate::socia
