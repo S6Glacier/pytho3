c7ea3953919af4be808793526619b47f752d8de9
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-05-31 06:53:14 +0000

    Resources

--- crates/apps/iwt/src/cross_publisher/mastodon/mod.rs
+++ crates/apps/iwt/src/cross_publisher/mastodon/mod.rs
@@ -5,4 +5,5 @@ use super::syndicated_post::SyndicatedPost;
 use super::target::Target;
 use crate::commons::{text, url_shortener};
 use crate::social::Network;
-use async_trait::asy
+use async_trait::async_trait;
+use futures::TryFutureEx
