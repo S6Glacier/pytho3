6d637e82a6beecf78aa07e5549d35041c900bf1b
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-07-09 12:48:32 +0000

    Update constants.php

--- crates/apps/iwt/src/cross_publisher/mastodon/mod.rs
+++ crates/apps/iwt/src/cross_publisher/mastodon/mod.rs
@@ -59,4 +59,4 @@ impl<WHClient: url_shortener::Client> Target for Mastodon<WHClient> {
             .await?;
 
         let status = text::shorten_with_permashort_citation(
-           
+            post.description
