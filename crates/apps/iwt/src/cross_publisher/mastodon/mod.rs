470a398b30d5a9c1a2e10d260697116b238380ae
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-07-30 09:00:21 +0000

    Added translation using Weblate (Swedish)

--- crates/apps/iwt/src/cross_publisher/mastodon/mod.rs
+++ crates/apps/iwt/src/cross_publisher/mastodon/mod.rs
@@ -76,4 +76,4 @@ impl<WHClient: url_shortener::Client> Target for Mastodon<WHClient> {
             .send()
             .map_err(|err| Box::new(err) as Box<dyn std::error::Error>)
             .and_then(|response| async {
-                let body = response.t
+                let body = response.text().await.expect("Response body expect
