523169d9ef135611d5c8adadafeeee6be5908c93
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-07-31 11:35:18 +0000

    regulator: wm831x: Set the new rather than old value for DVS VSEL

--- crates/apps/iwt/src/cross_publisher/mastodon/mod.rs
+++ crates/apps/iwt/src/cross_publisher/mastodon/mod.rs
@@ -79,4 +79,4 @@ impl<WHClient: url_shortener::Client> Target for Mastodon<WHClient> {
                 let body = response.text().await.expect("Response body expected");
 
                 serde_json::from_str::<MastodonResponse>(&body)
-                    .map(|res
+                    .map(|response| SyndicatedPost::
