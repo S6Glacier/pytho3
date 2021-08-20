d277a675f6bebf1e750d25b640242b7467b1fa4a
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-08-20 15:25:39 +0000

    update gitignores

--- crates/apps/iwt/src/cross_publisher/rss.rs
+++ crates/apps/iwt/src/cross_publisher/rss.rs
@@ -7,4 +7,7 @@ pub struct ReqwestClient;
 pub trait Client {
     /// Loads RSS feed from the given URL a parse it into a Channel
     async fn get_channel(&self, url: &str)
-        -> Result<Channel, Box<dyn std::error::Error + '
+        -> Result<Channel, Box<dyn std::error::Error + 'static>>;
+}
+
+#[async_t
