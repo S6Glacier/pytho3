15b6fcdd8266b4a1cbd53801d571fdb47bdcb163
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2021-12-26 22:38:12 +0000

    Review feedback.

--- crates/apps/iwt/src/cross_publisher/rss.rs
+++ crates/apps/iwt/src/cross_publisher/rss.rs
@@ -136,4 +136,7 @@ pub mod stubs {
     pub struct RssClientError;
     impl Display for RssClientError {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            write!(f, "Rss
+            write!(f, "RssClientError")
+        }
+    }
+  
