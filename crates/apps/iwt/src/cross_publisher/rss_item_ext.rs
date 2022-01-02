e2fc77948d6fb42d720af4db81feaa5947c6eb5e
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2022-01-02 09:29:48 +0000

    Add coverage badge

--- crates/apps/iwt/src/cross_publisher/rss_item_ext.rs
+++ crates/apps/iwt/src/cross_publisher/rss_item_ext.rs
@@ -18,3 +18,33 @@ pub struct IwtRssExtension {
 pub struct IwtRssTargetNetwork {
     pub network: social::Network,
 }
+
+pub trait RssItemExt {
+    fn get_iwt_extension(&self) -> Option<IwtRssExtension>;
+}
+
+fn get_children<'a>(ext: &'a Extension, key: &str) -> Vec<&'a Extension> {
+    ext.children()
+        .get(key)
+        .iter()
+        .flat_map(|children| children.iter())
+        .collect::<Vec<_>>()
+}
+
+fn get_key<'a>(ext: &'a Extension, key: &str) -> Option<&'a Extension> {
+    ext.children()
+        .get(key)
+        .and_then(|children| children.first())
+}
+
+fn get_value<'a>(ext: &'a Extension, key: &str) -> Option<&'a str> {
+    get_key(ext, key).and_then(rss::extension::Extension::value)
+}
+
+impl RssItemExt for Item {
+    fn get_iwt_extension(&self) -> Option<IwtRssExtension> {
+        // todo!()
+        self.extensions()
+            .get(&"iwt".to_string())
+            .and_then(|iwt_root| iwt_root.get("extension").map(|extensions| &extensions[0]))
+            .map(|iwt_extension| {
