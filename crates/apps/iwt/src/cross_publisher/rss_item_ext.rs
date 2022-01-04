a285ba23c62e1eb3f57dce66f141f092174856ca
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2022-01-04 14:03:15 +0000

    Updated build system

--- crates/apps/iwt/src/cross_publisher/rss_item_ext.rs
+++ crates/apps/iwt/src/cross_publisher/rss_item_ext.rs
@@ -48,3 +48,37 @@ impl RssItemExt for Item {
             .get(&"iwt".to_string())
             .and_then(|iwt_root| iwt_root.get("extension").map(|extensions| &extensions[0]))
             .map(|iwt_extension| {
+                // println!("iwt: {:?}", iwt_extension);
+                let target_networks = get_key(iwt_extension, "targetNetworks")
+                    .iter()
+                    .flat_map(|target_networks| get_children(target_networks, "targetNetwork"))
+                    .map(|target_network| {
+                        let target_network_name = target_network.value().unwrap();
+                        match target_network_name {
+                            "twitter" => IwtRssTargetNetwork {
+                                network: social::Network::Twitter,
+                            },
+                            "mastodon" => IwtRssTargetNetwork {
+                                network: social::Network::Mastodon,
+                            },
+                            _ => panic!("Unknown netowrk: {target_network_name}"),
+                        }
+                    })
+                    .collect::<Vec<_>>();
+
+                let tags = get_children(iwt_extension, "tags")
+                    .iter()
+                    .flat_map(|tags| get_children(tags, "tag"))
+                    .map(|tag| tag.value().unwrap().to_string())
+                    .collect();
+
+                let content_warning =
+                    get_value(iwt_extension, "contentWarning").map(std::borrow::ToOwned::to_owned);
+
+                IwtRssExtension {
+                    target_networks,
+                    content_warning,
+                    tags,
+                }
+            })
+    }
