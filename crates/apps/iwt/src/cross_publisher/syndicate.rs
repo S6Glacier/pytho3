1c9f5e420e4037fada0b5c6e3a4f59a19a78bc9a
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2022-01-18 21:30:52 +0000

    Update io.py

--- crates/apps/iwt/src/cross_publisher/syndicate.rs
+++ crates/apps/iwt/src/cross_publisher/syndicate.rs
@@ -16,3 +16,36 @@ pub async fn syndicate<R, S>(
     storage: &S,
     dry_run: bool,
 ) -> Result<(), Box<dyn std::error::Error>>
+where
+    R: rss::Client,
+    S: syndicated_post::Storage,
+{
+    log::debug!("Received config: {:?}", config);
+    run_and_collect(config.rss.urls.iter(), |url| {
+        rss_client
+            .get_channel(url)
+            .and_then(|channel| syndycate_channel(channel, targets, storage, dry_run))
+    })
+    .await
+}
+
+/// Syndicates a single channel
+async fn syndycate_channel<S: syndicated_post::Storage>(
+    channel: Channel,
+    targets: &[Box<dyn Target>],
+    storage: &S,
+    dry_run: bool,
+) -> Result<(), Box<dyn std::error::Error>> {
+    run_and_collect(targets.iter(), |target| {
+        run_and_collect(channel.items.iter(), |post| {
+            log::info!(
+                "{} |> Syndicating post to {}",
+                post.link().unwrap(),
+                target.network().to_string()
+            );
+            let stored = storage.find(&post.guid.as_ref().unwrap().value, &target.network());
+
+            // println!("Post: {:?}", post);
+
+            async {
+                match stored {
