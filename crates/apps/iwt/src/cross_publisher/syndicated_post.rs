f93622709dc63e1e23a042f342cea893f0f65142
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2022-02-23 16:23:25 +0000

    update mapping

--- crates/apps/iwt/src/cross_publisher/syndicated_post.rs
+++ crates/apps/iwt/src/cross_publisher/syndicated_post.rs
@@ -8,4 +8,5 @@ use crate::social::Network;
 #[derive(Debug, PartialEq, Clone)] // TODO: Clone is only needed for the tests
 pub struct SyndicatedPost {
     pub social_network: Network,
-    pu
+    pub id: String,
+    pub original_guid: S
