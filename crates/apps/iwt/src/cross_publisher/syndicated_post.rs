e53673ddbfe78a6911858e47b3b51d5b0bd3075c
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2022-02-26 10:04:05 +0000

    Bumping the version

--- crates/apps/iwt/src/cross_publisher/syndicated_post.rs
+++ crates/apps/iwt/src/cross_publisher/syndicated_post.rs
@@ -17,4 +17,5 @@ impl SyndicatedPost {
     pub fn new(social_network: Network, id: &str, item: &Item) -> Self {
         Self {
             social_network,
-           
+            id: String::from(id),
+       
