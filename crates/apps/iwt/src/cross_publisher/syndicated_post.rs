a007c687c7f25938601f1b10bd7c5391a061f745
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2022-03-20 09:24:01 +0000

    Revise documentation

--- crates/apps/iwt/src/cross_publisher/syndicated_post.rs
+++ crates/apps/iwt/src/cross_publisher/syndicated_post.rs
@@ -85,4 +85,5 @@ impl Storage for SqliteSyndycatedPostStorage {
         self.conn
             .execute(
                 "INSERT INTO post (id, social_network, original_guid, original_uri) 
-                 VALUES (:id, :social_network, :original_gu
+                 VALUES (:id, :social_network, :original_guid, :original_url)",
+         
