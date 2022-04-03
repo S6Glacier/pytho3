5abd1dad359e00d91e07360525d2f59cfa0bfae9
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2022-04-03 08:15:35 +0000

    v5.6.0

--- crates/apps/iwt/src/cross_publisher/syndicated_post.rs
+++ crates/apps/iwt/src/cross_publisher/syndicated_post.rs
@@ -115,4 +115,5 @@ impl Storage for SqliteSyndycatedPostStorage {
                 &[
                     (":original_guid", original_guid),
                     (":social_network", social_network.to_string().as_str()),
-      
+                ],
+                
