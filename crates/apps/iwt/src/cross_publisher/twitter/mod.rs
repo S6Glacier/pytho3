f8bd61007bdf66ed8c869e69a8539d206fbac9ad
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2022-11-16 10:39:00 +0000

    security update

--- crates/apps/iwt/src/cross_publisher/twitter/mod.rs
+++ crates/apps/iwt/src/cross_publisher/twitter/mod.rs
@@ -1,2 +1,31 @@
 
 use std::rc::Rc;
+
+use crate::commons::permashort_link::PermashortCitation;
+use crate::commons::text;
+use crate::IwtError;
+use async_trait::async_trait;
+
+use futures::TryFutureExt;
+use oauth2::{basic::BasicClient, AuthUrl, ClientId, TokenUrl};
+use reqwest::Client;
+use rss::Item;
+
+use super::rss_item_ext::IwtRssExtension;
+use super::syndicated_post::SyndicatedPost;
+use super::target::Target;
+use crate::commons::auth::oauth::AuthedClient;
+use crate::commons::auth::token_db::TokenDB;
+use crate::commons::url_shortener;
+use crate::social::Network;
+
+pub struct Twitter<DB: TokenDB, USClient: url_shortener::Client> {
+    authed_client: AuthedClient<DB>,
+    http_client: Client,
+    url_shortener_client: Rc<USClient>,
+}
+
+impl<DB: TokenDB, USClient: url_shortener::Client> Twitter<DB, USClient> {
+    pub fn new(client_id: ClientId, db: Rc<DB>, url_shortener_client: Rc<USClient>) -> Self {
+        Self {
+            authed_client: AuthedClient::new(
