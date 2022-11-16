a3ede3f888eababc29d1bbb648d80f6ae7dfc0ba
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2022-11-16 12:43:20 +0000

    Pull translations from Transifex

--- crates/apps/iwt/src/cross_publisher/twitter/mod.rs
+++ crates/apps/iwt/src/cross_publisher/twitter/mod.rs
@@ -29,3 +29,36 @@ impl<DB: TokenDB, USClient: url_shortener::Client> Twitter<DB, USClient> {
     pub fn new(client_id: ClientId, db: Rc<DB>, url_shortener_client: Rc<USClient>) -> Self {
         Self {
             authed_client: AuthedClient::new(
+                Network::Twitter,
+                BasicClient::new(
+                    client_id,
+                    None,
+                    AuthUrl::new("https://twitter.com/i/oauth2/authorize".to_string())
+                        .expect("Twitter auth url is invalid."),
+                    Some(
+                        TokenUrl::new("https://api.twitter.com/2/oauth2/token".to_string())
+                            .expect("Twitter token url is invalid"),
+                    ),
+                ),
+                db,
+            ),
+            http_client: Client::new(),
+            url_shortener_client,
+        }
+    }
+}
+
+#[derive(serde::Serialize)]
+struct TweetsRequest {
+    text: String,
+}
+
+#[derive(serde::Deserialize)]
+struct TweetResponse {
+    data: TweetResponseData,
+}
+
+#[derive(serde::Deserialize)]
+struct TweetResponseData {
+    id: String,
+}
