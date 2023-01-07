9a5d8b453090cb63f2ddc7af69073eaa4d8ba771
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2023-01-07 21:00:15 +0000

    initial load

--- crates/apps/iwt/src/main.rs
+++ crates/apps/iwt/src/main.rs
@@ -71,4 +71,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let log_level = if cli.debug { Debug } else { Info };
     SimpleLogger::new().with_level(log_level).init().unwrap();
 
-    le
+    let config = Config::from_file(&cli.
