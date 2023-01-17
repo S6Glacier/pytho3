96145c4144287bf4ee99233f3cf3123661f3f888
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2023-01-17 19:40:42 +0000

    Cleaned up code a bit.

--- crates/apps/iwt/src/social/mod.rs
+++ crates/apps/iwt/src/social/mod.rs
@@ -13,3 +13,21 @@ pub enum Network {
 
 impl Display for Network {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Network::Twitter => write!(f, "twitter"),
+            Network::Mastodon => write!(f, "mastodon"),
+        }
+    }
+}
+
+impl FromSql for Network {
+    fn column_result(value: rusqlite::types::ValueRef<'_>) -> rusqlite::types::FromSqlResult<Self> {
+        value.as_str().and_then(|n| match n {
+            "twitter" => Ok(Network::Twitter),
+            "mastodon" => Ok(Network::Mastodon),
+            n => Err(FromSqlError::Other(Box::new(SqlConversionError {
+                message: format!("Unknown social network: {n}"),
+            }))),
+        })
+    }
+}
---
+++ crates/apps/iwt/src/stubs/auth/token_db.rs
@@ -0,0 +1,3 @@
+
+pub mod stubs {
+    use std::sync::Mutex;
