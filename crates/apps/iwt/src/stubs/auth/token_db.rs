ca8c6a6a7724b4ad8527619fc58f4416f508cb0e
S6Glacier <109094987+S6Glacier@users.noreply.github.com>
2023-01-19 11:50:30 +0000

    New translations ja-JP.yml (Dutch)

--- crates/apps/iwt/src/stubs/auth/token_db.rs
+++ crates/apps/iwt/src/stubs/auth/token_db.rs
@@ -1,3 +1,40 @@
 
 pub mod stubs {
     use std::sync::Mutex;
+
+    use oauth2::{AccessToken, RefreshToken};
+
+    use crate::commons::auth::token_db::TokenDB;
+    use crate::social::Network;
+
+    pub struct StubTokenDB {
+        access_token: Mutex<AccessToken>,
+        refresh_token: Mutex<RefreshToken>,
+    }
+
+    impl StubTokenDB {
+        #[must_use]
+        pub fn new() -> Self {
+            StubTokenDB {
+                access_token: Mutex::new(AccessToken::new(String::from("initial-access-token"))),
+                refresh_token: Mutex::new(RefreshToken::new(String::from("initial-refresh-token"))),
+            }
+        }
+    }
+
+    impl Default for StubTokenDB {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+
+    impl TokenDB for StubTokenDB {
+        fn get_access_token(
+            &self,
+            _social_network: &Network,
+        ) -> Result<oauth2::AccessToken, Box<dyn std::error::Error>> {
+            let guard = self.access_token.lock().unwrap();
+            Ok((*guard).clone())
+        }
+
+        fn get_refresh_token(
